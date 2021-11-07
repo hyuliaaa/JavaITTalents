9485a0309d31b36105b8e64337e28573aceedf2a
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-11-07 19:57:22 +0200

    Update BinarySearch2DArray.java

--- Searching/BinarySearch2DArray.java
+++ Searching/BinarySearch2DArray.java
@@ -7,18 +7,26 @@ public class BinarySearch2dArray {
                 {7,8,9},
         };
 
-        System.out.println(binarySearch(matrix,11,0,(matrix.length*matrix[0].length)-1));
+        int result  = binarySearch(matrix,6,0,(matrix.length*matrix[0].length-1));
+        if(result >=0){
+            System.out.println("Row "  + result/matrix.length);
+            System.out.println("Column "+ result%matrix.length);
+        }
+        else
+        {
+            //element not found
+        }
     }
 
-    static boolean binarySearch(int[][]matrix,int x, int start, int end){
+    static int binarySearch(int[][]matrix,int x, int start, int end){
         if(start>end){
-            return false;
+            return -1;
         }
         int middle = (start+end)/2;
         int row = middle/matrix.length;
         int column = middle%matrix.length;
         if(matrix[row][column]==x){
-            return true;
+            return middle;
         }
 
         if(x > matrix[row][column]){
